4693beb2d382d5a2c627649874fc21c406cee1cc
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-09-12 23:05:13 -0300

    Requisito 1 primeira tentativa de testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,26 @@
 require('../mocks/fetchSimulator');
+const { expect } = require('chai');
+const { it } = require('mocha');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui 
-  fail('Teste vazio');
+  it('Testa se é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Testa se ao passar o argumento computador o fetch é chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+  it('Testa se ao passar o argumento computador utiliza o endpoint correto', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(url);
+  })
+  it('Testa se ao passar o argumento computador o retorno é o objeto computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  })
+  it('Testa se não passar argumentos retorna um erro', async () => {
+    expect(await fetchProducts()).toThrow(computadorSearch);
+  })
 });
