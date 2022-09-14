7412e757b9f719aa622d90f8115878c16f0c6509
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-09-14 01:26:55 -0300

    Requisito 6 inicial

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,14 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('testa se ao executar saveCartItems com um cartItem o localStorae.setItem é chamado', async () => {
+    expect.assertions(1);
+    await saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  })
+  it('testa se ao executar saveCartItems com um cartItem o localStorae.setItem é chamado com os parametros', async () => {
+    expect.assertions(1);
+    await saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'cartItem');
+  })
 });
